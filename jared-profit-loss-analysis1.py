a141a7c764ab01d7108e20507cfe56591b4ee8dc
Julius Alvarado <javascript.uiux@gmail.com>
2019-04-02 11:41:10 -0700

    cost per piece mode figured out for jared

--- jared-profit-loss-analysis1.py
+++ jared-profit-loss-analysis1.py
@@ -3,6 +3,7 @@ import pandas as pd;
 
 from matplotlib import pyplot as plot;
 
+
 jared_data = pd.read_csv('jared-data.csv');
 jared_profit_loss = jared_data['profit_loss_amount'];
 jared_profit_loss_mean = np.average(jared_profit_loss);
@@ -18,12 +19,26 @@ print(jared_median_profit_loss);
 print('jared sorted profit loss');
 print(jared_profit_loss_sorted);
 
-plot.hist(jared_profit_loss, range=(-100000, 100000), bins=20, edgecolor='red');
+plot.hist(jared_profit_loss, range=(-10000, 10000), bins=20, edgecolor='red');
 plot.title("Profit Loss data from Jareds' sales");
 plot.xlabel('Sales Amount');
 plot.ylabel('Count');
-plot.axvline(jared_profit_loss_mean, color='r', linestyle='solid', linewidth=2, label="Mean");
-plot.axvline(jared_median_profit_loss, color='g', linestyle='dotted', linewidth=2, label='Median');
+plot.axvline(
+	jared_profit_loss_mean,
+	color='r',
+	linestyle='solid',
+	linewidth=2,
+	label="Mean"
+);
+
+plot.axvline(
+	jared_median_profit_loss,
+	color='g',
+	linestyle='dotted',
+	linewidth=2,
+	label='Median'
+);
+
 plot.legend();
 
 plot.show();
---
+++ jared-profit-loss-mode.py
@@ -0,0 +1,44 @@
+import numpy as np;
+import pandas as pd;
+from scipy import stats;
+from matplotlib import pyplot as plot;
+
+jared_data = pd.read_csv('jared-data.csv');
+
+# Read in cost per click data
+jared_cost_per_click = jared_data['price_per_click'];
+
+# Use scipy to calculate the median age of cost per click
+mode_cost_per_click = stats.mode(jared_cost_per_click);
+mode_cost_per_click_raw = mode_cost_per_click[0][0];
+mode_cost_per_click_amount = mode_cost_per_click[1][0];
+median_cost_per_click = np.median(jared_cost_per_click);
+mean_cost_per_click = np.average(jared_cost_per_click);
+
+print("The mode = " + str(mode_cost_per_click_raw)
+      + ", amout of times it occurs = " + str(mode_cost_per_click_amount));
+
+
+plot.hist(jared_cost_per_click, range=(0.000, 0.200), bins=14, edgecolor='red');
+plot.title("Jared's cost per piece data");
+plot.xlabel("Price Per Piece");
+plot.ylabel("Count");
+plot.axvline(
+	mean_cost_per_click, color='r', linestyle='solid', linewidth=3, label="Mean"
+);
+plot.axvline(
+	median_cost_per_click, color='y', linestyle='dotted', linewidth=3, label="Median"
+);
+plot.axvline(
+	mode_cost_per_click_raw, color='orange', linestyle='dashed', linewidth=3, label="Mode"
+);
+plot.legend();
+plot.show();
+
+
+
+
+
+
+
+##
