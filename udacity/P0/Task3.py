120af4348d52fc1c87814dbb83b36d8ebc9000be
Julius Hernandez <julius_hernandez@apple.com>
2021-06-25 19:03:37 -0700

    Part A of the project done

--- udacity/P0/Task3.py
+++ udacity/P0/Task3.py
@@ -3,7 +3,10 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+import re
+import pprint
 
+pretty = pprint.PrettyPrinter(indent=4).pprint
 
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -20,6 +23,7 @@ TASK 3:
 Fixed line numbers include parentheses, so Bangalore numbers
 have the form (080)xxxxxxx.)
 
+
 Part A: Find all of the area codes and mobile prefixes called by people
 in Bangalore. In other words, the calls were initiated by "(080)" area code
 to the following area codes and mobile prefixes:
@@ -35,7 +39,34 @@ Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
+"""
 
+i_calling_num = 0
+i_receiving_num = 1
+i_start_time = 2
+i_duration = 3
+list_of_codes = {'fixed': set(), 'mobile': set(), 'telemarketer': set()}
+for i in calls:
+    call_num = i[i_calling_num]
+    receiving_num = i[i_receiving_num]
+    if call_num[:5] == '(080)':
+        fixed = re.search(r'\(\d+\)', receiving_num)
+        mobile = re.search(r'[789]\d+\s', receiving_num)
+        telemarketer = receiving_num[:3]
+        if fixed:
+            list_of_codes['fixed'].add(fixed.group())
+        elif mobile:
+            list_of_codes['mobile'].add(mobile.group().strip())
+        elif telemarketer == '140':
+            list_of_codes['telemarketer'].add(telemarketer)
+
+codes = []
+for k in list_of_codes:
+    codes.extend([i for i in list_of_codes[k]])
+
+print(f'The numbers called by people in Bangalore have codes:\n\n', pretty(codes))
+
+"""
 Part B: What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
@@ -46,3 +77,5 @@ Print the answer as a part of a message::
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
+debug = 1
