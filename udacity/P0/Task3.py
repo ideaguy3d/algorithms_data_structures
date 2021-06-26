a8eb2ef48e904619814f4754acce63a196828584
Julius Hernandez <julius_hernandez@apple.com>
2021-06-25 22:11:46 -0700

    Part B is done

--- udacity/P0/Task3.py
+++ udacity/P0/Task3.py
@@ -24,7 +24,8 @@ Fixed line numbers include parentheses, so Bangalore numbers
 have the form (080)xxxxxxx.)
 
 
-Part A: Find all of the area codes and mobile prefixes called by people
+                ~ Part A ~ 
+Find all of the area codes and mobile prefixes called by people
 in Bangalore. In other words, the calls were initiated by "(080)" area code
 to the following area codes and mobile prefixes:
  - Fixed lines start with an area code enclosed in brackets. The area
@@ -39,35 +40,10 @@ Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
-"""
 
-i_calling_num = 0
-i_receiving_num = 1
-i_start_time = 2
-i_duration = 3
-list_of_codes = {'fixed': set(), 'mobile': set(), 'telemarketer': set()}
-for i in calls:
-    call_num = i[i_calling_num]
-    receiving_num = i[i_receiving_num]
-    if call_num[:5] == '(080)':
-        fixed = re.search(r'\(\d+\)', receiving_num)
-        mobile = re.search(r'[789]\d+\s', receiving_num)
-        telemarketer = receiving_num[:3]
-        if fixed:
-            list_of_codes['fixed'].add(fixed.group())
-        elif mobile:
-            list_of_codes['mobile'].add(mobile.group().strip())
-        elif telemarketer == '140':
-            list_of_codes['telemarketer'].add(telemarketer)
 
-codes = []
-for k in list_of_codes:
-    codes.extend([i for i in list_of_codes[k]])
-
-print(f'The numbers called by people in Bangalore have codes:\n\n', pretty(codes))
-
-"""
-Part B: What percentage of calls from fixed lines in Bangalore are made
+                ~ Part B ~ 
+What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
 were made to a number also starting with "(080)"?
@@ -77,5 +53,50 @@ Print the answer as a part of a message::
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
+i_calling_num = 0
+i_receiving_num = 1
+i_start_time = 2
+i_duration = 3
+list_of_codes = {
+    'fixed': {'count': 0, 'code': set()},
+    'mobile': {'count': 0, 'code': set()},
+    'telemarketer': {'count': 0, 'code': set()}
+}
+bangalore_stat = {
+    'bangalore_count': 0,
+    'all_codes_total': 0
+}
+for i in calls:
+    call_num = i[i_calling_num]
+    receiving_num = i[i_receiving_num]
+    if call_num[:5] == '(080)':
+        fixed = re.search(r'\(\d+\)', receiving_num)
+        mobile = re.search(r'[789]\d+\s', receiving_num)
+        telemarketer = receiving_num[:3]
+        if fixed:
+            fixed = fixed.group()
+            list_of_codes['fixed']['code'].add(fixed)
+            list_of_codes['fixed']['count'] += 1
+            if fixed == '(080)':
+                bangalore_stat['bangalore_count'] += 1
+        elif mobile:
+            list_of_codes['mobile']['code'].add(mobile.group().strip())
+            list_of_codes['mobile']['count'] += 1
+        elif telemarketer == '140':
+            list_of_codes['telemarketer']['code'].add(telemarketer)
+            list_of_codes['telemarketer']['count'] += 1
+
+codes = []
+for k in list_of_codes:
+    code_type_list = [i for i in list_of_codes[k]['code']]
+    bangalore_stat['all_codes_total'] += list_of_codes[k]['count']
+    codes.extend(code_type_list)
+
+percentage = round(bangalore_stat['bangalore_count'] / bangalore_stat['all_codes_total'], 2)
+
+print(f'The numbers called by people in Bangalore have codes:\n', codes)
+print(f'\n\n{percentage} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.')
+
 
 debug = 1
