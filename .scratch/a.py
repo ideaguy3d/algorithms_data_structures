60acc0cb322697dd01dab82f7e3b8e3b544ba9de
Julius Hernandez <julius_hernandez@apple.com>
2021-09-06 03:04:15 -0700

    Create a.py

---
+++ .scratch/a.py
@@ -0,0 +1,15 @@
+from collections import deque
+from datetime import datetime
+import bisect
+
+q = deque([(0, 0)])
+
+
+def _date_practice():
+    d1 = datetime.now()
+    print(d1)
+
+
+_date_practice()
+
+#
