7e6eed124af03f73581576c62ae8db9e3b585a19
Julius Hernandez <julius_hernandez@apple.com>
2021-09-18 16:58:49 -0700

    union set by rank

--- @_Graphs/g1.py
+++ @_Graphs/g1.py
@@ -66,13 +66,25 @@ class UnionFind2:  # Quick Union
 
 class UnionFind:  # Quick Union by Rank
     def __init__(self, size):
-        pass
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
 
     def find(self, x):
-        pass
+        while x != self.root[x]:
+            x = self.root[x]
+        return x
 
     def union(self, x, y):
-        pass
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.root[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.root[root_x] = root_y
+            else:
+                self.root[root_y] = root_x
+                self.rank[root_x] += 1
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
@@ -88,12 +100,13 @@ uf.union(6, 7)
 uf.union(3, 8)
 uf.union(8, 9)
 # 4 is an island since it was not unioned to anything
-
 print(uf.connected(1, 5))  # true
 print(uf.connected(5, 7))  # true
 print(uf.connected(4, 9))  # false
 
-
+# 1-2-5-6-7 3-8-9-4
+uf.union(9, 4)
+print(uf.connected(4, 9))  # true
 
 
 
