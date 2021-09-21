7f9d556373375745f7c675f2d7e8a4aeeb58a185
Julius Hernandez <julius_hernandez@apple.com>
2021-09-21 01:06:09 -0700

    Update 167.py

--- leet/167.py
+++ leet/167.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 
-class Solution:
+class Solution1:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i, outer in enumerate(nums):
             for j, inner in enumerate(nums):
@@ -23,5 +23,28 @@ class Solution2:
                 left += 1
             else:
                 right -= 1
+
+
+class Solution:
+    @staticmethod
+    def two_sum(nums, target):
+        """
+        2_pointer algorithm
+        nums - a sorted ascending list
+        """
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            _sum = nums[left] + nums[right]
+            if target == _sum:
+                return left + 1, right + 1
+            if target < _sum:
+                right -= 1
+            else:
+                left += 1
+        return -1
+
+print(Solution.two_sum([1, 2, 3, 4, 5], 5))
+
 
 #
