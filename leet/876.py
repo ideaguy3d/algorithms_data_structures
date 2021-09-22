b51bc3b85cea9229d89f820883af46a645bc2547
Julius Hernandez <julius_hernandez@apple.com>
2021-09-22 00:51:49 -0700

    1st implementation from memory

--- leet/876.py
+++ leet/876.py
@@ -3,13 +3,30 @@ from typing import Optional
 
 # ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: ListNode{val: 5, next: None}}}}}
 class ListNode:
-    def __init__(self, val=0, next = None):
+    def __init__(self, val=0, _next=None):
         self.val = val
-        self.next = next
+        self.next = _next
 
 
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+class Solution1:
+    @staticmethod
+    def middleNode(head: Optional[ListNode], n) -> Optional[ListNode]:
+        size = 1
+        cur = p = head
+        while cur.next:
+            size += 1
+            cur = cur.next
+            if size > n + 1:
+                p = p.next
+        if size == n:
+            return head.next
+        else:
+            p.next = p.next.next
+            return head
+
+nodes = ListNode(5)
+print(Solution1.middleNode(nodes, 2))
+
+
 
 #
