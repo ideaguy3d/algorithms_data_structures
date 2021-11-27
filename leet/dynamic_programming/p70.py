36ec49b3a3277bb594ff96ee1a5f924cec0ba691
Julius Hernandez <julius_hernandez@apple.com>
2021-11-27 10:26:50 -0800

    problem 70 solution 3 with unit test

---
+++ leet/dynamic_programming/p70.py
@@ -0,0 +1,65 @@
+"""
+topics
+- dynamic programming
+- combinations & permutations
+"""
+import unittest
+
+
+class Solution1:
+    """
+    recursion
+    - space: O(n)
+    - time:  O(2^n)
+    """
+
+    def climbStairs(self):
+        pass
+
+
+class Solution2:
+    """
+    recursion with memoization
+    - space: O(n)
+    - time:  O(n)
+    """
+
+    def climbStairs(self):
+        pass
+
+
+class Solution3:
+    """
+    dynamic programming
+    - space: O(n)
+    - time:  O(n)
+    """
+    @staticmethod
+    def climbStairs(n: int) -> int:
+        if n == 1: return 1
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+
+class Test_Solutions(unittest.TestCase):
+    def test_s1(self):
+        pass
+
+    def test_s2(self):
+        pass
+
+    def test_s3(self):
+        s3 = Solution3
+        self.assertEqual(s3.climbStairs(6), 13, 'input 6 returns 13')
+        self.assertEqual(s3.climbStairs(2), 2, 'input 2 returns 2')
+        self.assertEqual(s3.climbStairs(3), 3, 'input 3 returns 3')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# end of file
