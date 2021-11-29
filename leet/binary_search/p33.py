3435d1a426006a5dd50600c8e3ffbf5ac1976697
Julius Hernandez <julius_hernandez@apple.com>
2021-11-29 04:15:37 -0800

    prepped problem 33

---
+++ leet/binary_search/p33.py
@@ -0,0 +1,43 @@
+"""
+topic
+- Binary Search
+"""
+import unittest
+from typing import List
+
+
+class Solution1:
+    """
+    binary search
+    - space:
+    - time:
+    """
+
+    def search(self, nums: List[int], target: int) -> int:
+        # TODO: implement solution1
+        pass
+
+
+class Solution2:
+    """
+    one pass binary search
+    - space: O(1)
+    - time:  O(logN)
+    """
+    def search(self, nums: List[int], target: int) -> int:
+        pass
+
+
+
+class Test_Solutions(unittest.TestCase):
+    def test_s1(self):
+        pass
+
+    def test_s2(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
+
+# end of file
