add55e2c20cc502cf8b1a759e03151aa001888a5
Julius Hernandez <julius_hernandez@apple.com>
2022-02-24 19:29:09 -0800

    p1 from mem2, w/unittest

--- leet/apple_interview/arrays_strings/p1.py
+++ leet/apple_interview/arrays_strings/p1.py
@@ -1,8 +1,16 @@
 """
 twoSum
+https://leetcode.com/problems/two-sum/
 """
-from typing import List
+from typing import List, Tuple
 import unittest
+
+
+# template
+class Solution:
+    @staticmethod
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        pass
 
 
 class Solution1a:
@@ -16,6 +24,18 @@ class Solution1a:
             _map[num] = i
 
 
+# template
+class Solution1b:
+    @staticmethod
+    def twoSum(nums: List[int], target: int) -> Tuple[int, int]:
+        m = {}
+        for i, n in enumerate(nums):
+            c = target - n  # mf
+            if c in m: return i, m[c]
+            m[n] = i
+
+
+
 class TestSolutions(unittest.TestCase):
     """
     [2,7,11,15]
@@ -26,6 +46,13 @@ class TestSolutions(unittest.TestCase):
         result1 = s.twoSum([2, 7, 11, 15], 9)
         print("result1 = ", result1)
         self.assertIn(result1, [[1, 0], (1, 0), (0, 1)], 'test_s1a assertion 1, result in container')
+
+    def test_s1b(self):
+        solution = Solution1b
+        result1 = solution.twoSum([2, 7, 11, 15], 9)
+        print("result1 = ", result1)
+        self.assertIn(result1, [[1, 0], (1, 0), (0, 1)], 'test_s1b assertion 1, result in container')
+
 
 
 if __name__ == '__main__':
