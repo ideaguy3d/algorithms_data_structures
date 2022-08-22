6f6fba659713994709b9f761f70d410bf62af288
Julius Hernandez <julius_hernandez@apple.com>
2022-08-21 23:36:31 -0700

    bash array practice

---
+++ sh_prac/arr.zsh
@@ -0,0 +1,27 @@
+# Declare associative array with key-value pairs on one line
+declare -A model_metrics=([model_accuracy]=98 [model_name]="knn" [model_f1]=0.82)
+
+# Print out the entire array
+echo ${model_metrics[@]}
+# "!" doesn't work :/ 
+# echo ${!model_metrics[@]}
+
+
+# Create variables from the temperature data files
+temp_b="$(cat temps/region_B)"
+temp_c="$(cat temps/region_C)"
+
+# Create an array with these variables as elements
+region_temps=($temp_b $temp_c)
+
+# Call an external program to get average temperature
+average_temp=$(echo "scale=2; (${region_temps[0]} + ${region_temps[1]}) / 2" | bc)
+
+# Append average temp to the array
+region_temps+=($average_temp)
+
+# Print out the whole array
+echo ${region_temps[@]}
+
+
+# eof #
