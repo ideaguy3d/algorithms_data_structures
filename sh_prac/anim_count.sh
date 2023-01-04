81bd554307b78dec797feb7a4d3ccd22c5799217
Julius Hernandez <julius_hernandez@apple.com>
2023-01-04 03:13:41 -0800

    changed folder name

--- sh_prac/anim_count.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Animal Counts:"
-cat animals.csv | cut -d " " -f 2 | sort | uniq -c 
--- sh_prac/eg.sh
+++
@@ -1,4 +0,0 @@
-echo "starting computation..."
-cat f2.txt | egrep 'carrot|lime' | sort
-cat f2.txt | egrep 'carrot|lime' | wc -l 
-echo "computation complete."
